e64b793f8e1b92997881cc04a484d52675eba2ed
Michael Bogdanov <michaelbogdanov@Michaels-MacBook-Pro.local>
2022-11-08 13:50:50 +0200

    updated mile/kilo

--- lib/screen_one.dart
+++ lib/screen_one.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'logic.dart';
 
+void main() => runApp(const MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
+
 
 class Calculator extends StatefulWidget {
   const Calculator({Key? key}) : super(key: key);
