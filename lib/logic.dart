3c38cd88da6df143031bf23766010778f82ff1f2
Michael Bogdanov <michaelbogdanov@Michaels-MacBook-Pro.local>
2022-11-09 19:41:34 +0200

    updated mile/kilo

--- lib/logic.dart
+++ lib/logic.dart
@@ -2,6 +2,8 @@ import 'package:calc/screen_two.dart';
 import 'package:flutter/material.dart';
 import 'formulas.dart';
 
+// the logic for the calculator is in the screen_one.dart file
+
 class ToSecondPage {
   static void func(BuildContext context) =>
       Navigator.push(
